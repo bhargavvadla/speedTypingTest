ca2c885003fd05042fb8a3f8ec8b52db035926c4
Bhargav Vadla <bhargavvadla4b6@gmail.com>
2021-10-26 07:28:51 +0530

    SpeedTypingTest
    Simple Web application for speed typing test

---
+++ main.js
@@ -0,0 +1,62 @@
+let quoteInputEl = document.getElementById('quoteInput');
+let quoteDisplayEl = document.getElementById('quoteDisplay');
+let submitBtnEl = document.getElementById('submitBtn');
+let resetBtnEl = document.getElementById('resetBtn');
+let resultEl = document.getElementById('result');
+let timerEl = document.getElementById('timer');
+let spinnerEl = document.getElementById('spinner');
+
+let randomQuote = "";
+
+
+function getRandomQuote() {
+    spinnerEl.classList.remove("d-none");
+    let url = "https://apis.ccbp.in/random-quote";
+    let options = {
+        method: "GET"
+    }
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonData) {
+            spinnerEl.classList.add("d-none");
+            let {
+                content
+            } = jsonData;
+            quoteDisplayEl.textContent = content;
+        })
+}
+
+getRandomQuote();
+quoteDisplayEl.textContent = randomQuote;
+
+let setId = null;
+let seconds = 1;
+
+function startTimer() {
+    seconds = 1;
+    clearInterval(setId);
+    quoteInputEl.textContent = "";
+    timerEl.textContent = "1";
+    setId = setInterval(function() {
+        seconds += 1;
+        timerEl.textContent = seconds;
+    }, 1000)
+}
+
+function checkAnswer() {
+    let quote = quoteDisplayEl.textContent;
+    let userInputQuote = quoteInputEl.value;
+    if (userInputQuote === quote) {
+        resultEl.textContent = "You typed in " + seconds + " seconds";
+        clearInterval(setId);
+    } else {
+        resultEl.textContent = "You typed incorrect sentence";
+    }
+
+}
+
+
+submitBtnEl.addEventListener("click", checkAnswer);
+resetBtnEl.addEventListener("click", startTimer);
